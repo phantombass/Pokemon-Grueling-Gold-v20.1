c71b830b3670ef7a7e9e1b83a7f8b3d406d94c72
phantombass <tmkeaton@ngu.edu>
2023-09-24 21:57:46 -0400

    0.3.7

--- new_files.sh
+++ new_files.sh
@@ -1,12 +1,15 @@
 #!/Data/Updater/bin/sh
 version=`cat version`
 files=`cat Data/Changes/${version}`
-curl -o Data/Changes/${version}/changes https://raw.githubusercontent.com/phantombass/Pokemon-Grueling-Gold-v20.1/Release/${files}/changes
-curl -o Data/Changes/${version}/deleted https://raw.githubusercontent.com/phantombass/Pokemon-Grueling-Gold-v20.1/Release/${files}/deleted
-curl -o "version_temp" https://raw.githubusercontent.com/phantombass/Pokemon-Grueling-Gold-v20.1/master/version
-link=`cat version_temp`
-echo 'mkfile() { mkdir -p "$(dirname "$1")" && touch "$1" ;  }' >> ~/.bashrc
-source ~/.bashrc
-mkfile Data/Changes/${link}/changes
-mkfile Data/Changes/${link}/deleted
-rm version_temp
+vers=`cat Data/Changes/versions`
+
+for version in ${vers}
+do
+	before="${version}"
+	echo 'mkfile() { mkdir -p "$(dirname "$1")" && touch "$1" ;  }' >> ~/.bashrc
+	source ~/.bashrc
+	mkfile Data/Changes/${before}/changes
+	mkfile Data/Changes/${before}/deleted
+	curl -o Data/Changes/${version}/changes https://raw.githubusercontent.com/phantombass/Pokemon-Grueling-Gold-v20.1/Release/${files}/changes
+	curl -o Data/Changes/${version}/deleted https://raw.githubusercontent.com/phantombass/Pokemon-Grueling-Gold-v20.1/Release/${files}/deleted
+done
